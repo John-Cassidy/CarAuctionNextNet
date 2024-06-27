6a555b9e2aef368acba2d87f42f4a5319ad621a4
John Cassidy <jpcassidy@outlook.com>
2024-06-27 09:18:21 -0400

    feat: Update AuctionController to include UpdateAuction endpoint

--- backend/src/Auction/AuctionService/Controllers/AuctionsController.cs
+++ backend/src/Auction/AuctionService/Controllers/AuctionsController.cs
@@ -61,4 +61,31 @@ public class AuctionsController : ControllerBase
         return CreatedAtRoute(nameof(GetAuctionById),
             new { id = auction.Id }, _mapper.Map<AuctionDto>(auction));
     }
+
+    // UpdateAuction
+    [HttpPut("{id}", Name = "UpdateAuction")]
+    public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+    {
+        var auction = await _context.Auctions
+        .Include(x => x.Item)
+        .FirstOrDefaultAsync(x => x.Id == id);
+        if (auction == null)
+        {
+            return NotFound();
+        }
+        // TODO: Check Seller == Username
+
+        auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+        auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+        auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+        auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+        auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+
+        var result = await _context.SaveChangesAsync() > 0;
+        if (!result)
+        {
+            return BadRequest("Could not save changes to the DB");
+        }
+        return Ok();
+    }
 }
