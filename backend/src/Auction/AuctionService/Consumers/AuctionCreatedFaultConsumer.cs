a92f8c57f1218d523ea37be3dd1afdf6ef416c35
John Cassidy <jpcassidy@outlook.com>
2024-07-06 11:16:13 -0400

    feat: Add AuctionCreatedFaultConsumer to AuctionService and handle invalid item model
    This commit adds the AuctionCreatedFaultConsumer to the AuctionService, allowing it to handle invalid item models. Specifically, if the item model is set to "Foo", an ArgumentException is thrown to prevent selling cars with that name. This ensures data integrity and prevents incorrect items from being saved.

---
+++ backend/src/Auction/AuctionService/Consumers/AuctionCreatedFaultConsumer.cs
@@ -0,0 +1,45 @@
+﻿using AuctionService.Data;
+using Contracts;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Consumers;
+
+public class AuctionCreatedFaultConsumer : IConsumer<Fault<AuctionCreated>>
+{
+    private readonly AuctionDbContext _context;
+
+    public AuctionCreatedFaultConsumer(AuctionDbContext context)
+    {
+        _context = context;
+    }
+    public async Task Consume(ConsumeContext<Fault<AuctionCreated>> context)
+    {
+        Console.WriteLine("--> Consuming faulty creation");
+
+        var exception = context.Message.Exceptions.First();
+
+        if (exception.ExceptionType == typeof(ArgumentException).FullName)
+        {
+            Console.WriteLine($"MassTransit:ArgumentException: {exception.Message}. Republish with different Model");
+
+            // Republish with different Model
+            var updatedModel = context.Message.Message.Model + "Bar";
+            var auction = await _context.Auctions
+                .Include(x => x.Item)
+                .FirstOrDefaultAsync(x => x.Item.Model == context.Message.Message.Model);
+            if (auction != null)
+            {
+                auction.Item.Model = updatedModel;
+                await _context.SaveChangesAsync();
+            }
+
+            context.Message.Message.Model = updatedModel;
+            await context.Publish(context.Message.Message);
+        }
+        else
+        {
+            Console.WriteLine($"Unhandled exception: {exception.Message}");
+        }
+    }
+}
