7b2bf7f5f51538ff4713fade5aa45af74d7f678e
John Cassidy <jpcassidy@outlook.com>
2024-07-10 10:18:44 -0400

    feat: Add AuctionFinishedConsumer and BidPlacedConsumer
    This commit adds the `AuctionFinishedConsumer` and `BidPlacedConsumer` classes to the `AuctionService` project. These consumers implement the `IConsumer` interface from the MassTransit library and handle the `AuctionFinished` and `BidPlaced` messages respectively. The consumers retrieve the relevant auction from the database based on the message's `AuctionId` and update the auction's status and other properties accordingly.

---
+++ backend/src/Auction/AuctionService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,37 @@
+﻿using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    private readonly AuctionDbContext _dbContext;
+
+    public BidPlacedConsumer(AuctionDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("--> Consuming bid placed");
+
+        var auction = await _dbContext.Auctions.FindAsync(Guid.Parse(context.Message.AuctionId));
+
+        // checks if the current high bid is either null 
+        // or if the received bid is marked as "Accepted" and its amount is higher than the current high bid. 
+        // If either condition is true, the auction's current high bid is updated to the amount of the received bid, 
+        // and changes are saved to the database with SaveChangesAsync.
+        if (auction.CurrentHighBid == null
+            || (
+                context.Message.BidStatus.Contains("Accepted")
+                && context.Message.Amount > auction.CurrentHighBid)
+            )
+        {
+            auction.CurrentHighBid = context.Message.Amount;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
