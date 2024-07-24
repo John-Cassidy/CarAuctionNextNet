82d06d46c39a175d4279808aad8cf380c73fe129
John Cassidy <jpcassidy@outlook.com>
2024-07-24 15:35:44 -0400

    refactor: Add IPublishEndpoint to BidsController constructor
    This commit adds the IPublishEndpoint parameter to the constructor of the BidsController in order to enable publishing of the BidPlaced event using MassTransit. This enhancement improves the functionality of the BidsController by integrating it with the messaging system.

--- backend/src/Bidding/BiddingService/Mappers/MappingProfiles.cs
+++ backend/src/Bidding/BiddingService/Mappers/MappingProfiles.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BiddingService.DTOs;
 using BiddingService.Models;
+using Contracts;
 
 namespace BiddingService.Mappers;
 
@@ -9,5 +10,6 @@ public class MappingProfiles : Profile
     public MappingProfiles()
     {
         CreateMap<Bid, BidDto>();
+        CreateMap<Bid, BidPlaced>();
     }
 }
