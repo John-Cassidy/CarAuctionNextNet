16991e34bcb95e925a651484c60fe48bc1aae99e
John Cassidy <jpcassidy@outlook.com>
2024-07-30 12:02:18 -0400

    feat: add AuctionCreatedToast

--- frontend/web-app/app/layout.tsx
+++ frontend/web-app/app/layout.tsx
@@ -4,24 +4,26 @@ import type { Metadata } from 'next';
 import Navbar from './nav/NavBar';
 import SignalRProvider from './providers/SignalRProvider';
 import ToasterProvider from './providers/ToasterProvider';
+import { getCurrentUser } from './actions/authActions';
 
 export const metadata: Metadata = {
   title: 'Car Auction App',
   description: 'A simple car auction app',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const user = await getCurrentUser();
   return (
     <html lang='en'>
       <body>
         <ToasterProvider />
         <Navbar />
         <main className='container mx-auto px-5 pt-10'>
-          <SignalRProvider>{children}</SignalRProvider>
+          <SignalRProvider user={user}>{children}</SignalRProvider>
         </main>
       </body>
     </html>
